716cc8a6affeffdde8b0f300d58b44d0997c8ec0
maitaoriana@gmail.com <hello@oca.com>
2018-02-28 20:52:12 +0000

    [ADD] Optic: company data added
    [ADD] Optic: Add company stock config

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+{
+    'name' : 'Optic Telecom',
+    'version' : '0.1',
+    'summary': 'App of Optic Telecom',
+    'description': "",
+    'depends': ['stock'],
+    'data': [
+	'data/stock_config.xml',
+	'data/res_company_data.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
